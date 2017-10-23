03d62129dc82b376d0573ffc8670b2efcec673f1
zxjsdp <zxjsdp@gmail.com>
2017-10-24 00:24:04 +0800

    Update usage

--- config/common.go
+++ config/common.go
@@ -7,6 +7,6 @@ const USAGE = `
 1. 直接双击 specimen-go 文件以运行用户界面程序；
 2. 在命令行中通过参数运行：
 
-	./specimen-go -q query.xlsx -d data.xlsx -o output.xlsx
+   ./specimen-go -query query.xlsx -data data.xlsx -output output.xlsx
 
 `
